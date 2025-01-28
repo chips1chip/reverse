427af7294ac594dc7f585920e3be66c858265818
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:52:51 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+
 public class Reverse {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter string");
-        String a=sc.nextLine();
-        String b="";
-        for(int i =a.length()-1;i>=0;i--){
-            b=b+a.charAt(i);
+        Scanner sc = new Scanner(System.in); // Scanner for user input
+        System.out.println("Enter a string:"); 
+        String input = sc.nextLine(); 
+        String reversed = ""; // Initialize an empty string 
+
+        // Loop through the string in reverse order
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i); // Append the characters in reverse order
         }
-        System.out.println("reverse of the string:"+b);
+
+        System.out.println("Reversed string: " + reversed); // Output the reversed string
+        sc.close(); 
     }
-    
 }
